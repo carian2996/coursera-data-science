3654c820611bf8f1f77dec9f3f3f228bd7ce633a
Ian Castillo Rosales <carian2996@gmail.com>
2014-06-08 14:50:17 -0500

    Full hw

--- r_programming_quiz1.R
+++ r_programming_quiz1.R
@@ -11,4 +11,21 @@ x4 <- 2:3
 
 class(x3 + x4)
 
-data <- 
+data <- read.csv("hw1_data.csv")
+data[1:2, ]
+
+nrow(data)
+data[(nrow(data)-1):nrow(data), ]
+
+data$Ozone[47]
+
+sum(is.na(data$Ozone))
+
+mean(data$Ozone[!is.na(data$Ozone)])
+
+data_new <- data[complete.cases(data$Ozone, data$Temp, data$Solar.R), ]
+mean(data_new$Solar.R[data_new$Ozone > 31 & data_new$Temp > 90])
+
+mean(data$Temp[ data$Month == 6 ])
+
+max(data$Ozone[ data$Month==5 & !is.na(data$Ozone) ])
