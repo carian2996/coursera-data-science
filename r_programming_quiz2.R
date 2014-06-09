2aeacd29882f8607b5cd2f5c6396d33db51ef90f
Ian Castillo Rosales <carian2996@gmail.com>
2014-06-09 17:59:28 -0500

    Quiz 2
    R Programming quiz

---
+++ r_programming_quiz2.R
@@ -0,0 +1,20 @@
+# Ian Castillo Rosales
+# R Programming
+# 09062014
+
+cubo <- function(x, n){
+  x^3
+}
+
+cubo(3)
+
+f <- function(x) {
+  g <- function(y) {
+    y + z
+  }
+  z <- 4
+  x + g(x)
+}
+
+z <- 10
+f(3)
