2553fb7bb1ce3776c6d1818126e0092db8016d5c
carian2996 <carian2996@gmail.com>
2014-10-15 19:17:24 -0500

    Week 2

--- Getting and Cleaning Data/assignment_2/run_analysis.R
+++ Getting and Cleaning Data/assignment_2/run_analysis.R
@@ -1,48 +1,65 @@
 # Ian Castillo Rosales
-# 27092014
+# 14102014
 
-setwd("~/Desktop/repos/datasciencecoursera/Getting and Cleaning Data/assignment_2")
+# Set the working directory in the place where you're going to work
+dir_work <- "~/Desktop/repos/datasciencecoursera/Getting and Cleaning Data/assignment_2"
+setwd(dir_work)
 
-# Download and unzip the data in the workspace
-if(!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")){
+require(dplyr)
+
+# ========== Download the data ==========
+if(!file.exists("data.zip")){
       url_file <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
       download.file(url_file, "data.zip")
 }
-unzip("data.zip")
 
-# ========== Read the train data ==========
+# Unzip the data if the data isn't already unziped
+if(!file.exists("UCI HAR Dataset")){
+      unzip("data.zip") 
+}
+
+# ========== Read the data ==========
 data_train <- read.table("./UCI HAR Dataset/train/x_train.txt")
 label_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-# ========== Read the test data ==========
 data_test <- read.table("./UCI HAR Dataset/test/x_test.txt")
 label_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 
-# ========== Join the data ==========
-data_joined <- rbind(data_train, data_test)
-label_joined <- rbind(label_train, label_test)
-subject_joined <- rbind(subject_train, subject_test)
+# ========== 1 Merge the data ==========
+data_joined <- rbind(data_train, data_test) # Join the train and test data in one set
+label_joined <- rbind(label_train, label_test) # Join the train and test labels in one set
+subject_joined <- rbind(subject_train, subject_test) # Join the train and test subjects in one set
 
-# ========== Extracting the mean and std measurement ==========
+# ========== 2 Extracting the mean and std measurements ==========
+# Load the features from the file and make it readable
 features <- read.table("./UCI HAR Dataset/features.txt")
-mean_index <- grep("mean\\(\\)|std\\(\\)", features[, 2])
+features[, 2] <- gsub("\\(\\)", "", features[, 2]) # remove "()"
+features[, 2] <- gsub("-", " ", features[, 2]) # remove "-" in column names 
 
+# Extract only features releated with mean or standard deviation
+mean_index <- grep("mean | std", features[, 2])
+
+# Keep only the features releated with mean or standard deviation
 data_joined <- data_joined[, mean_index]
 
-names(data_joined) <- gsub("\\(\\)", "", features[mean_index, 2]) # remove "()"
-names(data_joined) <- gsub("-", "", names(data_joined)) # remove "-" in column names 
-
-# ========== Name the activities in the data set ==========
+# ========== 3 Name the activities and subjects in the data set ==========
 activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
+label_joined <- tolower(activity[label_joined[, 1], 2])
 
-label_joined[, 1] <- activity[label_joined[, 1], 2]
-names(label_joined) <- "activity"
+# Merge the sets into an only tidy data set
+tidy_data <- cbind(subject_joined, label_joined, data_joined)
+tidy_data <- arrange(tidy_data, Subject, Activity)
 
-# ========== Labels the data set with descriptive variable names ==========
-names(label_joined) <- "subject"
-cleanedData <- cbind(subject_joined, label_joined, data_joined)
+# Name the variables with descriptive names
+names(tidy_data) <- c("Subject", "Activity", as.character(features[mean_index, 2]))
+# tidy_data[, 1] <- apply(tidy_data[,1], 1, function(x) paste("Subject", x))
 
-# ========== Write the text file ==========
-write.table(cleanedData, "merged_data.txt")
+# ========== 4 Write the text file ==========
+write.table(tidy_data, "tidy_data.txt")
+
+# ========== Create a new set of data ==========
+new_data <- tidy_data
+unique(tidy_data$Subject)
+
