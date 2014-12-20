98f05cb978673739f5265c4f221965c01e302c74
carian2996 <carian2996@gmail.com>
2014-12-20 14:43:33 -0600

    quiz 3

--- Regression Models/quizz3.R
+++ Regression Models/quizz3.R
@@ -5,55 +5,52 @@
 # Question 1
 data(mtcars)
 fit <- lm(mpg ~ factor(cyl) + wt, mtcars)
-summary(fit)
+coef(fit)[3]
 
 # Question 2
 data(mtcars)
-
 fit1 <- lm(mpg ~ factor(cyl) + wt, data = mtcars)
 fit2 <- lm(mpg ~ factor(cyl), data = mtcars)
-
-fit2$coefficients[3]
+summary(fit1)
+summary(fit2)
+# Holding weight constant, cylinder appears to have less of an impact on mpg than if weight is disregarded.
 
 # Question 3
 data(mtcars)
-
 fit1 <- lm(mpg ~ factor(cyl) + wt, data = mtcars)
 fit2 <- lm(mpg ~ factor(cyl) + wt + interaction(cyl, wt), data = mtcars)
-
 # To compare model we usually use an anova table
 # anova null hypothesis says that both models are the same.
-compare <- anova(fit1, fit2)
-compare$Pr
+anova(fit1, fit2)
+# The P-value is larger than 0.05. So, according to our criterion, 
+# we would fail to reject, which suggests that the interaction may not be necessary.
 
 # Question 4
 # Consider the mtcars data set. 
 # Fit a model with mpg as the outcome that includes number of cylinders as a 
 # factor variable and weight inlcuded in the model as
 lm(mpg ~ I(wt * 0.5) + factor(cyl), data = mtcars)
-# How is the wt coefficient interpretted?
-
-
 # note : ton = 2 tonnes
-# The estimated expected change in MPG per one ton increase in weight for a specific number of cylinders (4, 6, 8).
+# The estimated expected change in MPG per one ton increase in weight 
+# for a specific number of cylinders (4, 6, 8).
 
-# Question 5
+# Question 5 FAIL
 # Consider the following data set
-
 x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
 y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
-
 # Give the hat diagonal for the most influential point
-
 fit <- lm(y ~ x)
-hatvalues(fit)
+hatvalues(fit) # 0.9946
 
-# Question 6
+# Question 6 FAIL
 # Consider the following data set
-
 x <- c(0.586, 0.166, -0.042, -0.614, 11.72)
 y <- c(0.549, -0.026, -0.127, -0.751, 1.344)
 # Give the slope dfbeta for the point with the highest hat value.
-
 fit <- lm(y ~ x)
-dfbetas(fit)
+dfbetas(fit) # 134
+
+# Question 7
+# It is possible for the coefficient to reverse sign after adjustment. 
+# For example, it can be strongly significant and positive before adjustment 
+# and strongly significant and negative after adjustment.
