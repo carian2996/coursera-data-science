a1e5dd31c5d7119bb22c94107485425474ae4f34
Ian Castillo Rosales <carian2996@gmail.com>
2015-07-13 23:46:52 -0500

    Upload
    Getting the data and tokenization.

--- Capstone Project/Week1/getting_data.R
+++ Capstone Project/Week1/getting_data.R
@@ -8,13 +8,15 @@
 # Load tm package. Loading required NLP package
 library(NLP)
 library(tm)
-source("~/Coursera/count_nrows.R")
+source("~/Coursera/count_nrows.R") # Windows
 
 # Set a local working directory (depends of you)
-setwd("~/Coursera/data/en_US/")
+setwd("~/Coursera/data/en_US/") # Windows
+setwd("~/Documents/datasciencecoursera/Capstone Project/data/en_US/") #Mac
 
-# Create a rute for your Corpus
-dirCorpus <-  DirSource("~/Coursera/data/en_US/")
+# Create a rute for your Corpus (Optional)
+dirCorpus <- DirSource("~/Coursera/data/en_US/") # Windows
+dirCorpus <- DirSource("~/Documents/datasciencecoursera/Capstone Project/data/en_US/")
 corpus <- Corpus(dirCorpus, readerControl = list(language = "en"))
 
 # Summary what went in
@@ -25,15 +27,23 @@ summary(corpus)
 # Create a connection and read certains lines of the corpus
 
 sample_creation <- function(sample_size, path){
+    con <- file(path)
+    nrow_text <- as.numeric(system("wc -l < en_US.news.txt", intern = T))
     
+    text_sample <- c()
+    n_sample <- 0
+    for(i in 1:nrow_text){
+        test <- rbinom(1, 1, prob = c(0.5, 0.5))
+        if(test == 1){
+            n_sample <- n_sample + 1
+            readed_line <- scan(file = con, what = "char", nlines = 1, 
+                                skip = i-1, quiet = T)
+            text_sample <- c(text_sample, readed_line)
+        }
+        if(n_sample == sample_size) break
+    }
+    close(con)
+    paste(text_sample, collapse = " ")
 }
 
-sample_size <- 1000
-path <- "en_US.twitter.txt"
-
-con <- file(path)
-nrow_text <- numrow_windows("en_US.twitter.txt")
-
-readLines(con, nrow_text)
-?scan
-
+sample <- sample_creation(sample_size = 10, path = "~/Documents/datasciencecoursera/Capstone Project/data/en_US/en_US.news.txt")
