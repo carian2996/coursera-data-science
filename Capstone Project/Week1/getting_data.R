e227a259c72ee71afd5c95e84f875cc5bdbcfa88
Ian Castillo Rosales <carian2996@gmail.com>
2015-07-16 14:10:59 -0500

    Upload
    Ya pude crear la muestra del corpus con su respectiva partición. La transformo de nuevo a Corpus pero ya no puedo hacer los análisis porque aparecen demasiados caracteres raros.

--- Capstone Project/Week1/getting_data.R
+++ Capstone Project/Week1/getting_data.R
@@ -19,49 +19,80 @@ setwd("~/Documents/datasciencecoursera/Capstone Project/data/en_US/") #Mac
 
 # Create a rute for your Corpus (Optional)
 dirCorpus <- DirSource("~/Coursera/data/en_US/") # Windows
-dirCorpus <- DirSource("~/Documents/datasciencecoursera/Capstone Project/data/en_US/")
-corpus <- Corpus(dirCorpus, readerControl = list(language = "en"))
+dirCorpus <- DirSource("~/Documents/datasciencecoursera/Capstone Project/data/en_US/") # Mac
 
-class(corpus)
+corpus <- Corpus(dirCorpus, readerControl = list(language = "en"))
 summary(corpus)
 
-muestra <- sample(x = corpus[[1]]$content, size = 10000, replace = F)
-head(muestra)
+inspect(corpus)
+class(corpus[[1]]$content)
+summary(corpus[[1]]$content)
+head(corpus[[1]]$content)
+
+nrows_news <- as.numeric(summary(corpus[[1]]$content)[1])
+
+n_muestra <- 10000
+id_muestra <- sort(sample(x = 1:nrows_news, size = n_muestra, replace = F))
+
+muestra <- corpus[[1]]$content[id_muestra]
+
+muestraTrain <- muestra[id_muestra[createDataPartition(y = id_muestra, 
+                                               p = 0.7, list = F)]]
+muestraTest <- muestra[id_muestra[-createDataPartition(y = id_muestra, 
+                                               p = 0.7, list = F)]]
+
+CmuestraTrain <- Corpus(VectorSource(muestraTrain))
+CmuestraTest <- Corpus(VectorSource(muestraTest))
+
+
+getTransformations()
+?tm_map
+
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+
+CmuestraTrain <- tm_map(CmuestraTrain, toSpace, "/|@|\\|-")
+CmuestraTest <- tm_map(CmuestraTest, toSpace, "/|@|\\|-|")
+
+CmuestraTrain <- tm_map(CmuestraTrain, content_transformer(tolower))
+CmuestraTest <- tm_map(CmuestraTest, content_transformer(tolower))
+
+CmuestraTrain <- tm_map(CmuestraTrain, removeWords, stopwords("english"))
+CmuestraTest <- tm_map(CmuestraTest, removeWords, stopwords("english"))
+
+CmuestraTrain <- tm_map(CmuestraTrain, removeWords, c("shit"))
+CmuestraTest <- tm_map(CmuestraTest, removeWords, c("shit"))
+
+CmuestraTrain <- tm_map(CmuestraTrain, stripWhitespace)
+CmuestraTest <- tm_map(CmuestraTest, stripWhitespace)
+
+dtm_train <- DocumentTermMatrix(CmuestraTrain)
+
+CmuestraTrain[[1]]$content
+muestraTrain[2]
 
 # ===== Load a sample of the corpus =====
 
 # Create a connection and read certains lines of the corpus
 
-sample_creation <- function(sample_size, path){
-    con <- file(path)
-    nrow_text <- as.numeric(system("wc -l < en_US.news.txt", intern = T))
-    
-    text_sample <- c()
-    n_sample <- 0
-    for(i in 1:nrow_text){
-        test <- rbinom(1, 1, prob = c(0.5, 0.5))
-        if(test == 1){
-            n_sample <- n_sample + 1
-            readed_line <- scan(file = con, what = "char", nlines = 1, 
-                                skip = i-1, quiet = T)
-            text_sample <- c(text_sample, readed_line)
-        }
-        if(n_sample == sample_size) break
-    }
-    close(con)
-    paste(text_sample, collapse = " ")
-}
-
-sample <- sample_creation(sample_size = 10, path = "~/Documents/datasciencecoursera/Capstone Project/data/en_US/en_US.news.txt")
-
-# ===== Use createDataPartition to sample text =====
-con_en <- file ("en_US.news.txt")
-sample <- createDataPartition(y = readLines(con_en), times = 1, p = 0.8)
-
-
-?createDataPartition
-
-
-
-
+# sample_creation <- function(sample_size, path){
+#     con <- file(path)
+#     nrow_text <- as.numeric(system("wc -l < en_US.news.txt", intern = T))
+#     
+#     text_sample <- c()
+#     n_sample <- 0
+#     for(i in 1:nrow_text){
+#         test <- rbinom(1, 1, prob = c(0.5, 0.5))
+#         if(test == 1){
+#             n_sample <- n_sample + 1
+#             readed_line <- scan(file = con, what = "char", nlines = 1, 
+#                                 skip = i-1, quiet = T)
+#             text_sample <- c(text_sample, readed_line)
+#         }
+#         if(n_sample == sample_size) break
+#     }
+#     close(con)
+#     paste(text_sample, collapse = " ")
+# }
+# 
+# sample <- sample_creation(sample_size = 10, path = "~/Documents/datasciencecoursera/Capstone Project/data/en_US/en_US.news.txt")
 
