6c44d5eb56cf6dd0f2f2cd1e62ac73856c6f2092
Ian Castillo Rosales <carian2996@gmail.com>
2015-07-20 16:17:26 -0500

    Uplo

--- Capstone Project/Week1/getting_data.R
+++ Capstone Project/Week1/getting_data.R
@@ -2,97 +2,117 @@
 # Getting data and overview
 # 07/06/2015
 
+# ===== Options and libraries =====
+
 # OPTIONAL: Install framework package for text mining applications within R
 # install.packages(c("tm", "NPL", "caret"))
 
 # Load tm package. Loading required NLP package
-library(NLP); library(tm); library(caret)
-options(encoding = "UTF-8")
-source("~/Coursera/week1/count_nrows.R") # Windows
+library(NLP); library(tm); library(caret); library(SnowballC)
+library(wordcloud)
+getOption("encoding")
+
+# ===== Set the paths and directions =====
+
+# source("~/Coursera/week1/count_nrows.R") # Windows
 
 # Set a local working directory (depends of you)
-setwd("~/Coursera/data/en_US/") # Windows
-setwd("~/Documents/datasciencecoursera/Capstone Project/Week1/") #Mac
+setwd("~/Coursera/data/en_US") # Windows
+# setwd("~/Documents/datasciencecoursera/Capstone Project/Week1/") #Mac
 
 # Create a rute for your Corpus (Optional)
 dirCorpus <- DirSource("~/Coursera/data/en_US/") # Windows
-dirCorpus <- DirSource("~/Desktop/data_capstone/en_US/") # Mac
+# dirCorpus <- DirSource("~/Desktop/data_capstone/en_US/") # Mac
 
-corpus <- Corpus(x = dirCorpus, readerControl = list(language = "en"))
-
+# ===== Load the corpus in the workspace =====
+corpus <- Corpus(dirCorpus, readerControl = list(language = "en"))
 summary(corpus)
 
+# Explore the documents in the corpus
+summary(corpus[[1]]$content); summary(corpus[[2]]$content); summary(corpus[[3]]$content)
+
+# ===== Sample data =====
+
 # Make analysis with the blog text
-
-class(corpus[[1]]$content)
-summary(corpus[[1]]$content)
-head(corpus[[1]]$content)
-
+# Get the number of row in a document of the corpus
 nrows_news <- as.numeric(summary(corpus[[1]]$content)[1])
 
-n_muestra <- 1000
+# Set the value of the sample
+n_muestra <- 5000
 id_muestra <- sort(sample(x = 1:nrows_news, size = n_muestra, replace = F))
 
 muestra <- corpus[[1]]$content[id_muestra]
-head(muestra)
+# muestra <- paste(muestra[id_muestra], collapse = "")
 
+# Create a partition of the sample
 idPartition <- createDataPartition(y = id_muestra, p = 0.7, list = F)
-
-muestraTrain <- muestra[idPartition]
-muestraTest <- muestra[-idPartition]
+muestraTrain<- paste(muestra[idPartition], collapse = " ")
+muestraTest<- paste(muestra[-idPartition], collapse = " ")
 
 CmuestraTrain <- Corpus(VectorSource(muestraTrain))
 CmuestraTest <- Corpus(VectorSource(muestraTest))
 
 # ===== Tokenization of a text =====
 
-tokenization <- function(corpus){
-    bad_words <- read.csv("bad_words", header = F)
+source("~/Coursera/week1/tokenization.R")
+clean_sample <- tokenization(CmuestraTrain, "~/Coursera/week1/bad_words.txt")
+
+
+# ===== Exploratory analysis of the document =====
+
+qplot(x = names(tokens[[2]][1:15]), 
+      y = tokens[[2]][1:15], 
+      geom = "bar", 
+      stat = "identity",
+      xlab = "Most frenquent tokens",
+      ylab = "Frequency",
+      main = "Some words are more frequent than others?"
+      )
+
+tdm_sample <- TermDocumentMatrix(clean_sample[[3]])
+freq_sample <- sort(rowSums(as.matrix(tdm_sample)), decreasing = T)
+df_sample <- data.frame(words = names(freq_sample), freq = freq_sample)
+
+wordcloud(df_sample$words, 
+          df_sample$freq, 
+          random.order = F, 
+          scale=c(3.5,.5),
+          max.words = 50, 
+          colors = brewer.pal(6, "GnBu"))
+
+tokenization <- function(cor, dir_badwords){
+    # tokenization recive a corpus, clean the text; remove "/", "@" and "-".
+    # transform all text to lowercase, remove punctuation, stopwords and "bad 
+    # words", strip whitespaces in the text and finally, tokenize the text and 
+    # make a frequency of the words.
     
+    # INPUT
+    # corpora = Text to analyze (should be in corpus class)
+    # dir_badwords = Directory with the bad words to remove. Should be text file.
     
+    # OUTPUT
+    # tokens = tokens and frequency
+    
+    # Read the list of bad words
+    bad_words <- readLines(dir_badwords)
+    
+    # Create a function to convert a pattern into whitespace
     toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
     
-    CmuestraTrain <- tm_map(CmuestraTrain, toSpace, "/|@|-|\\(DOT\\)")
-    CmuestraTrain <- tm_map(CmuestraTrain, content_transformer(tolower))
-    CmuestraTrain <- tm_map(CmuestraTrain, removePunctuation)
-    CmuestraTrain <- tm_map(CmuestraTrain, removeWords, stopwords("english"))
-    CmuestraTrain <- tm_map(CmuestraTrain, removeWords, c("shit"))
-    CmuestraTrain <- tm_map(CmuestraTrain, stripWhitespace)
+    # Do the cleaning in the text
+    cor <- tm_map(cor, stemDocument)
+    cor <- tm_map(cor, toSpace, "/|@|-|â€\u009d|â€œ|â€™|â€”|â€|zz+|http:.|www.|.com")
+    cor <- tm_map(cor, content_transformer(tolower))
+    cor <- tm_map(cor, removePunctuation)
+    cor <- tm_map(cor, removeWords, stopwords("english"))
+    cor <- tm_map(cor, removeWords, bad_words)
+    cor <- tm_map(cor, stripWhitespace)
     
-    dtm_train <- DocumentTermMatrix(CmuestraTrain)
-    freq <- colSums(as.matrix(dtm_train))
-    tokens <- freq[order(freq, decreasing = T)]
+    tokens <- scan_tokenizer(cor[[1]]$content)
     
-    tokens
+    freq_tokens <- table(tokens)
+    freq_tokens <- freq_tokens[order(freq_tokens, decreasing = T)]
+    output <- list(tokens, freq_tokens, cor)
+    
+    output
 }
-
-
-
-
-
-# ===== Load a sample of the corpus =====
-
-# Create a connection and read certains lines of the corpus
-
-# sample_creation <- function(sample_size, path){
-#     con <- file(path)
-#     nrow_text <- as.numeric(system("wc -l < en_US.news.txt", intern = T))
-#     
-#     text_sample <- c()
-#     n_sample <- 0
-#     for(i in 1:nrow_text){
-#         test <- rbinom(1, 1, prob = c(0.5, 0.5))
-#         if(test == 1){
-#             n_sample <- n_sample + 1
-#             readed_line <- scan(file = con, what = "char", nlines = 1, 
-#                                 skip = i-1, quiet = T)
-#             text_sample <- c(text_sample, readed_line)
-#         }
-#         if(n_sample == sample_size) break
-#     }
-#     close(con)
-#     paste(text_sample, collapse = " ")
-# }
-# 
-# sample <- sample_creation(sample_size = 10, path = "~/Documents/datasciencecoursera/Capstone Project/data/en_US/en_US.news.txt")
-
