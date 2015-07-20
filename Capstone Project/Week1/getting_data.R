f239f80337f7845ab7740838a5540c8199be7d1f
Ian Castillo Rosales <carian2996@gmail.com>
2015-07-19 20:09:26 -0500

    Upload
    Casi lista la función para limpiar, quitar malas palabras y obtener
    tokens de un texto.

--- Capstone Project/Week1/getting_data.R
+++ Capstone Project/Week1/getting_data.R
@@ -2,10 +2,6 @@
 # Getting data and overview
 # 07/06/2015
 
-# Ian Castillo Rosales
-# Getting data and overview
-# 07/06/2015
-
 # OPTIONAL: Install framework package for text mining applications within R
 # install.packages(c("tm", "NPL", "caret"))
 
@@ -16,61 +12,63 @@ source("~/Coursera/week1/count_nrows.R") # Windows
 
 # Set a local working directory (depends of you)
 setwd("~/Coursera/data/en_US/") # Windows
-setwd("~/Documents/datasciencecoursera/Capstone Project/data/en_US/") #Mac
+setwd("~/Documents/datasciencecoursera/Capstone Project/Week1/") #Mac
 
 # Create a rute for your Corpus (Optional)
 dirCorpus <- DirSource("~/Coursera/data/en_US/") # Windows
-dirCorpus <- DirSource("~/Documents/datasciencecoursera/Capstone Project/data/en_US/") # Mac
+dirCorpus <- DirSource("~/Desktop/data_capstone/en_US/") # Mac
 
 corpus <- Corpus(x = dirCorpus, readerControl = list(language = "en"))
 
 summary(corpus)
 
-inspect(corpus)
+# Make analysis with the blog text
+
 class(corpus[[1]]$content)
 summary(corpus[[1]]$content)
 head(corpus[[1]]$content)
 
 nrows_news <- as.numeric(summary(corpus[[1]]$content)[1])
 
-n_muestra <- 10000
+n_muestra <- 1000
 id_muestra <- sort(sample(x = 1:nrows_news, size = n_muestra, replace = F))
 
 muestra <- corpus[[1]]$content[id_muestra]
+head(muestra)
 
-muestraTrain <- muestra[id_muestra[createDataPartition(y = id_muestra, 
-                                               p = 0.7, list = F)]]
-muestraTest <- muestra[id_muestra[-createDataPartition(y = id_muestra, 
-                                               p = 0.7, list = F)]]
+idPartition <- createDataPartition(y = id_muestra, p = 0.7, list = F)
+
+muestraTrain <- muestra[idPartition]
+muestraTest <- muestra[-idPartition]
 
 CmuestraTrain <- Corpus(VectorSource(muestraTrain))
 CmuestraTest <- Corpus(VectorSource(muestraTest))
 
+# ===== Tokenization of a text =====
 
-getTransformations()
-?tm_map
+tokenization <- function(corpus){
+    bad_words <- read.csv("bad_words", header = F)
+    
+    
+    toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+    
+    CmuestraTrain <- tm_map(CmuestraTrain, toSpace, "/|@|-|\\(DOT\\)")
+    CmuestraTrain <- tm_map(CmuestraTrain, content_transformer(tolower))
+    CmuestraTrain <- tm_map(CmuestraTrain, removePunctuation)
+    CmuestraTrain <- tm_map(CmuestraTrain, removeWords, stopwords("english"))
+    CmuestraTrain <- tm_map(CmuestraTrain, removeWords, c("shit"))
+    CmuestraTrain <- tm_map(CmuestraTrain, stripWhitespace)
+    
+    dtm_train <- DocumentTermMatrix(CmuestraTrain)
+    freq <- colSums(as.matrix(dtm_train))
+    tokens <- freq[order(freq, decreasing = T)]
+    
+    tokens
+}
 
-toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
 
-CmuestraTrain <- tm_map(CmuestraTrain, toSpace, "/|@|\\|-")
-CmuestraTest <- tm_map(CmuestraTest, toSpace, "/|@|\\|-|")
 
-CmuestraTrain <- tm_map(CmuestraTrain, content_transformer(tolower))
-CmuestraTest <- tm_map(CmuestraTest, content_transformer(tolower))
 
-CmuestraTrain <- tm_map(CmuestraTrain, removeWords, stopwords("english"))
-CmuestraTest <- tm_map(CmuestraTest, removeWords, stopwords("english"))
-
-CmuestraTrain <- tm_map(CmuestraTrain, removeWords, c("shit"))
-CmuestraTest <- tm_map(CmuestraTest, removeWords, c("shit"))
-
-CmuestraTrain <- tm_map(CmuestraTrain, stripWhitespace)
-CmuestraTest <- tm_map(CmuestraTest, stripWhitespace)
-
-dtm_train <- DocumentTermMatrix(CmuestraTrain)
-
-CmuestraTrain[[1]]$content
-muestraTrain[2]
 
 # ===== Load a sample of the corpus =====
 
